f2601d203d02b5607758d1844ca73bff7105a26d
Rhoda Pickens <rhodajane@gmail.com>
2023-04-03 06:20:24 -0500

    Final clean up

--- classes/student.js
+++ classes/student.js
@@ -19,18 +19,6 @@ class Student extends Person {
 
 }
 
-let student1 = new Student("billy", "johnson", "software-development", 3.2);
-console.log(student1.major);
-
-/* Implement the following in the classes/student.js file.
-Create a new Student class and have its constructor call the super(),
-passing the parameters needed for a Person and a constructor function that
-takes in additional parameters for their major subject and GPA.
-Check the specs to determine which parameters you need to inherit.
-Create a static method called compareGPA which will take in two Student instances
-as the first and second parameter and return a string comparing the GPAs return
-"<firstName> <lastName> has the higher GPA." If they're equal, return "Both students have the same GPA."
-
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
