edca7355e2c66eb050630149348a5599fde590cb
Rhoda Pickens <rhodajane@gmail.com>
2023-04-06 06:14:50 -0500

    Finished teacher class with method

--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,24 @@
 const Person = require('./person');
 
-// Your code here
+// constructor function that extends the Person class
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+  // static method to sum the combined years of experience of all the teachers in the array
+  static combinedYearsOfExperience(teachers) {  // array of teacher instances
+    let sum = 0;
+    for (let i = 0; i < teachers.length; i++) {
+      let teacher = teachers[i];
+      sum += teacher.yearsOfExperience;
+    }
+    return sum;
+  }
+
+}
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
